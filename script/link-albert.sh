e30292df7fe286686394ab644d50971ae45bba2e
Kadlecsik Tamás <tamas.kadlecsk@gmail.com>
2019-10-04 16:58:34 +0200

    rewrite for use with stow

--- script/link-albert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Usage: ./link-git.sh
-
-CURR_DIR=$(pwd)
-PROJECTS_DIR=~/Projects/src/github.com/Shadowbeetle/dotfiles
-cd ~/.config/albert
-ln -sf $PROJECTS_DIR/linux/dot-config/albert/albert.conf.symlink albert.conf
-cd $CURR_DIR
--- script/link-xmodmap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Usage: ./link-git.sh
-
-CURR_DIR=$(pwd)
-PROJECTS_DIR=~/Projects/src/github.com/Shadowbeetle/dotfiles
-cd ~
-ln -sf $PROJECTS_DIR/linux/dot-Xmodmap.symlink .Xmodmap
-cd $CURR_DIR
