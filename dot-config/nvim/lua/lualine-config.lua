ac8ae397f270a53bdbe65d49ad1327585d969183
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-07-11 20:28:15 +0200

    use location and progress

--- dot-config/nvim/lua/lualine-config.lua
+++ dot-config/nvim/lua/lualine-config.lua
@@ -3,9 +3,10 @@ require('lualine').setup {
         icons_enabled = true,
         theme = 'auto',
         --component_separators = { left = '', right = ''},
-        component_separators = { left = '╲', right = '╱' },
-        --section_separators = { left = '', right = ''},
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = '' },
         disabled_filetypes = { 'NvimTree' },
         always_divide_middle = true,
         globalstatus = false,
@@ -16,7 +17,7 @@ require('lualine').setup {
         lualine_c = {'filename'},
         lualine_x = {'encoding', 'fileformat'},
         lualine_y = {'filetype'},
-        lualine_z = {{'os.date("%-I:%M %p")', color = {gui='NONE'}}}
+        lualine_z = {'location', 'progress'}
     },
     inactive_sections = {
         lualine_a = {},
