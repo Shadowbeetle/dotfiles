33fc108a26ed6100779441860dfd1eac61f7d5b0
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-07-31 16:39:27 +0200

    use :RG for FzfLua grep

--- dot-config/nvim/lua/plug-opts/fzf.lua
+++ dot-config/nvim/lua/plug-opts/fzf.lua
@@ -4,3 +4,5 @@ vim.api.nvim_create_user_command("Gf", "FzfLua git_status", {})
 vim.api.nvim_create_user_command("B", "FzfLua buffers", { bang = true })
 vim.api.nvim_create_user_command("Com", "FzfLua commands", { bang = true })
 vim.api.nvim_create_user_command("Col", "FzfLua colorschemems", { bang = true })
+vim.api.nvim_create_user_command("RG", "FzfLua grep", { bang = true })
+
