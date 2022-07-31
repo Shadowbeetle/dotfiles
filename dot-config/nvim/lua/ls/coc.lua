1870ead93397b9a3f1fdae6877abd6675f2db55a
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-07-31 16:39:04 +0200

    use <c-space> to open suggestions

--- dot-config/nvim/lua/ls/coc.lua
+++ dot-config/nvim/lua/ls/coc.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>hd", ':call CocActionAsync("doHover")<cr>', {noremap = true, silent = true, unique = true})
+vim.api.nvim_set_keymap("i", "<c-space>", "coc#refresh()", {noremap = true, silent = true, expr = true})
