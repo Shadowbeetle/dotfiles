bddc0bd32711568161ac24cac1e82d1a04f6cbb4
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-08-03 14:14:04 +0000

    remove lua-rocks

--- dot-config/nvim/lua/load-lua-libs.lua
+++ dot-config/nvim/lua/load-lua-libs.lua
@@ -1,4 +1,4 @@
-local nvim_rocks = require'nvim_rocks'
+-- local nvim_rocks = require'nvim_rocks'
 
 -- Ensure that certain rocks are installed
-nvim_rocks.ensure_installed('lua-path')
+-- nvim_rocks.ensure_installed('lua-path')
